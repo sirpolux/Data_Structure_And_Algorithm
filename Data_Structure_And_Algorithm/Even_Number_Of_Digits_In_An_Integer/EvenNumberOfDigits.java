28ca450bee072244af31afb932fe81b6d5d9d032
sirpolux <poluxreloaded@gmail.com>
2023-11-14 00:47:58 +0100

    Even number of digits. Task initialization.

---
+++ Data_Structure_And_Algorithm/Even_Number_Of_Digits_In_An_Integer/EvenNumberOfDigits.java
@@ -0,0 +1,7 @@
+package Even_Number_Of_Digits_In_An_Integer;
+
+public class EvenNumberOfDigits {
+    public  static  void  main(String[] args){
+        System.out.println("I Should work");
+    }
+}
