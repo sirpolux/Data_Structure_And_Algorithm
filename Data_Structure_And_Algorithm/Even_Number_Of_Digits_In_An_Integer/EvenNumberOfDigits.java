8cbc0da83914c6d2415b657c5934b7dce64e7ec1
sirpolux <poluxreloaded@gmail.com>
2023-11-14 01:52:00 +0100

    Even number of digits. Algorithm Completed.

--- Data_Structure_And_Algorithm/Even_Number_Of_Digits_In_An_Integer/EvenNumberOfDigits.java
+++ Data_Structure_And_Algorithm/Even_Number_Of_Digits_In_An_Integer/EvenNumberOfDigits.java
@@ -2,13 +2,9 @@ package Even_Number_Of_Digits_In_An_Integer;
 
 public class EvenNumberOfDigits {
     public  static  void  main(String[] args){
-        int[] numSet = {647,7489,2,0,828,-939394,3939030,0};
-        int evenCount = 0;
-        for(int i:numSet){
-            if(evenDigits(len(i)))
-                evenCount++;
-        }
-        System.out.println(evenCount);
+        //Sample input
+        int[] numSet = {555,901,482,1771};
+        System.out.println(getEvenCount(numSet));
     }
     static boolean evenDigits(int len){
         return len%2==0;
@@ -20,4 +16,14 @@ public class EvenNumberOfDigits {
             return 1;
         return (int)Math.log10(num)+1;
     }
+
+    static int getEvenCount(int[] nums){
+        int evenCount = 0;
+        for(int i:nums){
+            if(evenDigits(len(i)))
+                evenCount++;
+        }
+        return  evenCount;
+    }
+
 }
