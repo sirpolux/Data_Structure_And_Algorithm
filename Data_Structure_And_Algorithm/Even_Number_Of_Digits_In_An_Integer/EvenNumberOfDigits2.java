0bf42b7fac236095db868a1fb83d2c661f9dbe9e
sirpolux <poluxreloaded@gmail.com>
2025-03-07 15:35:09 +0100

    update

--- Data_Structure_And_Algorithm/Even_Number_Of_Digits_In_An_Integer/EvenNumberOfDigits2.java
+++ Data_Structure_And_Algorithm/Even_Number_Of_Digits_In_An_Integer/EvenNumberOfDigits2.java
@@ -2,19 +2,25 @@ package Even_Number_Of_Digits_In_An_Integer;
 
 public class EvenNumberOfDigits2 {
     public static void main(String[] args) {
-
+        int [] sampleData = {12,345,2,6,7896};
     }
 
 
-
-
-
-    private static boolean isEven(int len){
-        return len%2==0;
+    static int findNumbers(int[] nums){
+        int evenCount =0;
+        for (int num:nums){
+            if (isEven(num)){
+                evenCount++;
+            }
+        }
+        return evenCount;
     }
 
-    private static int getSize(String num){
-        return (int)(Math.log10(Integer.parseInt(num))+1);
+   private static boolean isEven(int num){
+        return getSize(num)%2==0;
+    }
+    private static int getSize(int num){
+        return (int)(Math.log10(num)+1);
     }
 
 }
