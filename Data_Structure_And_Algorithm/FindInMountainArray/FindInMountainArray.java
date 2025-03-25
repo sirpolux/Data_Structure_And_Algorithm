03a4ebfab9f2fde112f809f867ad160be72070fa
sirpolux <poluxreloaded@gmail.com>
2025-03-25 09:22:11 +0100

    update

--- Data_Structure_And_Algorithm/FindInMountainArray/FindInMountainArray.java
+++ Data_Structure_And_Algorithm/FindInMountainArray/FindInMountainArray.java
@@ -6,10 +6,13 @@ public class FindInMountainArray {
         int[] arr1 = {1, 3, 5, 7, 9, 11}; // Ascending
         int[] arr2 = {10, 8, 6, 4, 2, 0}; // Descending
         int[] mountainArray = {1, 3, 7, 12,13, 10, 5, 2};
-        System.out.println(agnosticBinarySearch(arr1,0,arr1.length-1,11));
-        System.out.println(agnosticBinarySearch(arr2,0,arr2.length-1, 10));
+    //    System.out.println(agnosticBinarySearch(arr1,0,arr1.length-1,11));
+      //  System.out.println(agnosticBinarySearch(arr2,0,arr2.length-1, 10));
         //System.out.println(peakIndexMountainArray(mountainArray));
         MountainArray mountainArray1 = new MountainArray();
+
+        int peak = peakIndexMountainArray2(mountainArray1, 0, mountainArray1.getSize());
+       // System.out.println(agnosticBinarySearch2(mountainArray1,));
     }
 
     static  int agnosticBinarySearch(int[] arr, int start, int end, int target){
@@ -35,6 +38,44 @@ public class FindInMountainArray {
         return  -1;
 
     }
+
+    static  int agnosticBinarySearch2(MountainArray arr, int start, int end, int target){
+        while (start<=end){
+            int mid = start + (end-start)/2;
+            if(arr.getIndex(mid) == target){
+                return mid;
+            }
+            if (arr.getIndex(start)<arr.getIndex(end)){
+                if (target>arr.getIndex(mid)){
+                    start = mid+1;
+                }else {
+                    end = mid-1;
+                }
+            }else {
+                if (target>arr.getIndex(mid)){
+                    end=mid-1;
+                }else {
+                    start = mid+1;
+                }
+            }
+        }
+        return  -1;
+
+    }
+
+
+    static int peakIndexMountainArray2(MountainArray arr, int start, int end){
+        while (start<end){
+            int mid = start + (end-start)/2;
+            if (arr.getIndex(mid)>arr.getIndex(mid+1)){
+                end=mid;
+            }else {
+                start=mid+1;
+            }
+        }
+        return  start;
+    }
+
 
     static int peakIndexMountainArray(int[] arr, int start, int end){
         while (start<end){
@@ -65,5 +106,8 @@ class MountainArray{
             return -1;
         }
         return  mountainArray[index];
+    }
+    public int getSize(){
+        return mountainArray.length;
     }
 }
