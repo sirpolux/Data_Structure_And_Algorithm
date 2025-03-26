e32bc136b3dfc0d0c69679d99cc99c9c1ce59cd3
sirpolux <poluxreloaded@gmail.com>
2025-03-26 11:58:49 +0100

    update

---
+++ Data_Structure_And_Algorithm/RotatedBinarySearch/RBS.java
@@ -0,0 +1,15 @@
+package RotatedBinarySearch;
+
+public class RBS {
+    public static void main(String[] args) {
+        /**
+         * CONDITIONS FOR PEAK ELEMENT IN A ROTATED ARRAY
+         *
+         * 1. mid > mid + 1;
+         * 2. mid < mid -1
+         * 3. mid <= start  the peak should be between mib-1 and start
+         * 4. else  peak  will be between mid+1 to end
+         *
+         */
+    }
+}
